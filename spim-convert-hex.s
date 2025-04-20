d9bad268378759b7c09ecf411776fd81f07711fd
Ekali <ekali@ekali.fr>
2025-04-20 21:49:45 +0200

    refactor(indent): Weird indentation.

--- spim-convert-hex.s
+++ spim-convert-hex.s
@@ -3,19 +3,19 @@ hex:    .asciiz "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C",
         
         .text
 main:
-	    li $t0, 16
-        la $t3, hex
+    	li $t0, 16
+	la $t3, hex
 
-	    li $v0, 5
-	    syscall
+    	li $v0, 5
+    	syscall
 	
-	    or $a0, $zero, $v0
-	    j to_binary	
+    	or $a0, $zero, $v0
+    	j to_binary	
 
 to_binary:
-	    divu $a0, $t0 # LO = Quotient / HI = Reste
+    	divu $a0, $t0 # LO = Quotient / HI = Reste
 	
-	    mfhi $a0 # On bouge le reste dans $a0
+    	mfhi $a0 # On bouge le reste dans $a0
 
         sll $a0, $a0, 1 # mult par 2
         
@@ -24,14 +24,14 @@ to_binary:
         move $a0, $t4
 
         li $v0, 4
-	    syscall
+    	syscall
 
         mflo $a0
 
-	    beqz $a0, end_binary
-	    j to_binary
+    	beqz $a0, end_binary
+    	j to_binary
 
 end_binary:
-	    li $v0, 10
-	    syscall
+    	li $v0, 10
+    	syscall
 	
